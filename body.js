35ddb03f086b73f86b8337d33966fe16a235a7c7
massmediagroup-28 <b.zahurskyi@massmediagroup.pro>
2017-10-17 14:12:24 +0300

    add body

---
+++ body.js
@@ -0,0 +1,38 @@
+import AjaxForm from './ajax-form'
+
+export default class extends AjaxForm {
+    successSubmit(response) {
+        super.successSubmit(response);
+
+        if (!response.redirect) {
+            $('.js-customer-form-errors').html('');
+            $('.js-success-order-popup').show();
+
+            $('.js-cartTotalPrice').text('0');
+            $('.js_cart_items_count').text('0');
+            $('.js-to_order-button').hide();
+        }
+    }
+
+    errorSubmit(response) {
+        let $errorsBlock = $('.js-customer-form-errors');
+
+        if (response.status == 422) {
+            let $deliveryRadio = $('.js-purchase-delivery-radio');
+            let $paymentRadio = $('.js-purchase-payment-radio');
+
+            $.each(response.responseJSON.errors, function (i, v) {
+                if (i == 'delivery') {
+                    $deliveryRadio.siblings('label').find('span.box').addClass('radio-error');
+                }
+                if (i == 'payment') {
+                    $paymentRadio.siblings('label').find('span.box').addClass('radio-error');
+                }
+            });
+        }
+
+        if (response.status == 500) {
+            $errorsBlock.html('');
+        }
+    }
+}
